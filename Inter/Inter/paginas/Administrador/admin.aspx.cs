53b674f0104ec7f3fc472cfe5545158eca04615b
Levi Monteiro Junior <levigalaxys@gmail.com>
2015-03-12 17:38:00 -0300

    Commit de teste para o Nicolas

--- Inter/Inter/paginas/Administrador/admin.aspx.cs
+++ Inter/Inter/paginas/Administrador/admin.aspx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+//Estou comentando.
 public partial class Páginas_Administrador_admin : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
