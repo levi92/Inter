c310f0f6ab31aaa8308c3383dd860eaebda9d423
Levi Monteiro Junior <levigalaxys@gmail.com>
2015-05-12 17:14:18 -0300

    Solicitações

--- Inter/AppCode/Classes/Mensagem.cs
+++ Inter/AppCode/Classes/Mensagem.cs
@@ -1,47 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Text;
+using System.Threading.Tasks;
 
-
-public class Mensagem
+class Mensagem
 {
 
-    private int men_id;
-    private int men_id_enviado;
-    private DateTime men_hora;
-    private string men_corpo;
-    private int men_tic_id;
-
-
-    public int Men_id
-    {
-        get { return men_id; }
-        set { men_id = value; }
-    }
-
-    public int Men_id_enviado
-    {
-        get { return men_id_enviado; }
-        set { men_id_enviado = value; }
-    }
-
-    public DateTime Men_hora
-    {
-        get { return men_hora; }
-        set { men_hora = value; }
-    }
-
-    public string Men_corpo
-    {
-        get { return men_corpo; }
-        set { men_corpo = value; }
-    }
-
-    public int Men_tic_id
-    {
-        get { return men_tic_id; }
-        set { men_tic_id = value; }
-    }
-
+    public int Id { get; private set; } //CHAVE PRIMÁRIA.Identificador da Mensagem. Set privado porque o ID é autoincrementado no BD
+    public string Id_Enviado { get; set; } //Identificador do usuario que enviou a mensagem
+    public int Id_Ticket { get; set; } //Identificador do ticket ao qual a mensagem pertence
+    public DateTime Timestamp { get; set; } //Data e hora de envio da mensagem
+    public string Corpo { get; set; } //Conteúdo da mensagem 
 }
--- Inter/AppCode/Classes/Ticket.cs
+++ Inter/AppCode/Classes/Ticket.cs
@@ -2,56 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using AppCode;
 
 
 public class Ticket
 {
-
-    private int tic_id_professor, tic_estado, tic_prioridade, tic_id;
-    private string tic_assunto, tic_tipo;
-    private DateTime tic_data;
-
-
-    public int Tic_id
-    {
-        get { return tic_id; }
-        set { tic_id = value; }
-    }
-
-    public string Tic_assunto
-    {
-        get { return tic_assunto; }
-        set { tic_assunto = value; }
-    }
-
-    public int Tic_id_professor
-    {
-        get { return tic_id_professor; }
-        set { tic_id_professor = value; }
-    }
-
-    public int Tic_estado
-    {
-        get { return tic_estado; }
-        set { tic_estado = value; }
-    }
-
-    public DateTime Tic_data
-    {
-        get { return tic_data; }
-        set { tic_data = value; }
-    }
-
-    public string Tic_tipo
-    {
-        get { return tic_tipo; }
-        set { tic_tipo = value; }
-    }
-
-    public int Tic_prioridade
-    {
-        get { return tic_prioridade; }
-        set { tic_prioridade = value; }
-    }
-
+    //Métodos estão privados porque o set e get estão inline
+    public int Tic_Id { get; private set; } //CHAVE PRIMÁRIA.Identificador do Ticket. Set privado pois é autoincrementado no BD
+    public string Tic_Id_Professor { get; set; } // Identificador do professor que CRIOU o ticket (Usuário que criou)
+    public int Tic_Estado { get; set; } //Estado do Ticket: Não Aberto(0 - vermelho fatec), aberto(1 - azul), encerrado favorável(2 - verde), encerrado não favorável(3 - amarelo) 
+    public int Tic_Prioridade { get; set; } //Prioridade Normal (0 - padrão - ícone verde), média (1 - ícone amarelo), alta (2 - ícone vermelho)
+    public string Tic_Assunto { get; set; } //Assunto do ticket
+    public string Tic_Tipo { get; set; } //Tipo do Ticket. Categorias pré-definidas. (Usuário)
+    public DateTime Tic_Data { get; set; } //Data e hora da criação do Ticket
 }
