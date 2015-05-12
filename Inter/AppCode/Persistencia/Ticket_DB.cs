7a65d0d34a278a9f6eed68a3965bd59a26b5c013
Levi Monteiro Junior <levigalaxys@gmail.com>
2015-05-12 17:22:34 -0300

    Ticket

--- Inter/AppCode/Persistencia/Ticket_DB.cs
+++ Inter/AppCode/Persistencia/Ticket_DB.cs
@@ -18,12 +18,12 @@ public class Ticket_DB
             string sql = "INSERT INTO tic_ticket(tic_assunto, tic_id_professor, tic_data, tic_estado, tic_tipo, tic_prioridade) VALUES (?tic_assunto, ?tic_id_professor, ?tic_data, ?tic_estado, ?tic_tipo, ?tic_prioridade)";
             conexao = Mapped.Connection();
             objCommand = Mapped.Command(sql, conexao);
-            objCommand.Parameters.Add(Mapped.Parameter("?tic_assunto", ticket.Tic_assunto));
-            objCommand.Parameters.Add(Mapped.Parameter("?tic_id_professor", ticket.Tic_id_professor));
-            objCommand.Parameters.Add(Mapped.Parameter("?tic_data", ticket.Tic_data));
-            objCommand.Parameters.Add(Mapped.Parameter("?tic_estado", ticket.Tic_estado));
-            objCommand.Parameters.Add(Mapped.Parameter("?tic_tipo", ticket.Tic_tipo));
-            objCommand.Parameters.Add(Mapped.Parameter("?tic_prioridade", ticket.Tic_prioridade));
+            objCommand.Parameters.Add(Mapped.Parameter("?tic_assunto", ticket.Tic_Assunto));
+            objCommand.Parameters.Add(Mapped.Parameter("?tic_id_professor", ticket.Tic_Id_Professor));
+            objCommand.Parameters.Add(Mapped.Parameter("?tic_data", ticket.Tic_Data));
+            objCommand.Parameters.Add(Mapped.Parameter("?tic_estado", ticket.Tic_Estado));
+            objCommand.Parameters.Add(Mapped.Parameter("?tic_tipo", ticket.Tic_Tipo));
+            objCommand.Parameters.Add(Mapped.Parameter("?tic_prioridade", ticket.Tic_Prioridade));
             conexao.Close();
             objCommand.Dispose();
             conexao.Dispose();
@@ -46,13 +46,13 @@ public class Ticket_DB
             string sql = "UPDATE tic_ticket SET tic_assunto = ?tic_assunto, tic_id_professor = ?tic_id_professor, tic_data = ?tic_data, tic_estado = ?tic_estado, tic_tipo = ?tic_tipo, tic_prioridade = ?tic_prioridade WHERE tic_id = ?tic_id ";
             conexao = Mapped.Connection();
             objCommand = Mapped.Command(sql, conexao);
-            objCommand.Parameters.Add(Mapped.Parameter("?tic_assunto", ticket.Tic_assunto));
-            objCommand.Parameters.Add(Mapped.Parameter("?tic_id_professor", ticket.Tic_id_professor));
-            objCommand.Parameters.Add(Mapped.Parameter("?tic_data", ticket.Tic_data));
-            objCommand.Parameters.Add(Mapped.Parameter("?tic_estado", ticket.Tic_estado));
-            objCommand.Parameters.Add(Mapped.Parameter("?tic_tipo", ticket.Tic_tipo));
-            objCommand.Parameters.Add(Mapped.Parameter("?tic_prioridade", ticket.Tic_prioridade));
-            objCommand.Parameters.Add(Mapped.Parameter("?tic_id", ticket.Tic_id));
+            objCommand.Parameters.Add(Mapped.Parameter("?tic_assunto", ticket.Tic_Assunto));
+            objCommand.Parameters.Add(Mapped.Parameter("?tic_id_professor", ticket.Tic_Id_Professor));
+            objCommand.Parameters.Add(Mapped.Parameter("?tic_data", ticket.Tic_Data));
+            objCommand.Parameters.Add(Mapped.Parameter("?tic_estado", ticket.Tic_Estado));
+            objCommand.Parameters.Add(Mapped.Parameter("?tic_tipo", ticket.Tic_Tipo));
+            objCommand.Parameters.Add(Mapped.Parameter("?tic_prioridade", ticket.Tic_Prioridade));
+            objCommand.Parameters.Add(Mapped.Parameter("?tic_id", ticket.Tic_Id));
             objCommand.ExecuteNonQuery();
             conexao.Close();
             objCommand.Dispose();
@@ -105,12 +105,12 @@ public class Ticket_DB
             while (objDataReader.Read())
             {
                 objTicket = new Ticket();
-                objTicket.Tic_assunto = objDataReader["tic_assunto"].ToString();
-                objTicket.Tic_id_professor = Convert.ToInt32(objDataReader["tic_id_professor"]);
-                objTicket.Tic_data = DateTime.ParseExact(objDataReader["tic_data"].ToString(), "dd/MM/yyyy - HH:mm", null);
-                objTicket.Tic_estado = Convert.ToInt32(objDataReader["tic_estado"]);
-                objTicket.Tic_prioridade = Convert.ToInt32(objDataReader["tic_prioridade"]);
-                objTicket.Tic_tipo = objDataReader["tic_tipo"].ToString();
+                objTicket.Tic_Assunto = objDataReader["tic_assunto"].ToString();
+                objTicket.Tic_Id_Professor = (objDataReader["tic_id_professor"]).ToString();
+                objTicket.Tic_Data = DateTime.ParseExact(objDataReader["tic_data"].ToString(), "dd/MM/yyyy - HH:mm", null);
+                objTicket.Tic_Estado = Convert.ToInt32(objDataReader["tic_estado"]);
+                objTicket.Tic_Prioridade = Convert.ToInt32(objDataReader["tic_prioridade"]);
+                objTicket.Tic_Tipo = objDataReader["tic_tipo"].ToString();
             }
             objDataReader.Close();
             objConnection.Close();
